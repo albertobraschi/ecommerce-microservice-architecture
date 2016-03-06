f8aad977acabd3b697082a224cec4a93fa84663c
Salvo Zappalà <salvo.a.zappala@gmail.com>
2016-03-06 19:22:58 +0100

    Fix indentation

--- catalog/src/routes/products.js
+++ catalog/src/routes/products.js
@@ -9,20 +9,19 @@ const NEXT_PRODUCT_ID;
  * Add a new product
  */
 router.post('/', function (req, res) {
-  // get request parameters
-  var productData = {
-    title: req.param('title'),
-    price: req.param('price'),
-    description: req.param('description'),
-  };
-  // process data
-  var redisClient = req.redisClient;
-
-  var product = new Product(productData, redisClient);
-  product.save(function (entityId) {
-    res.status(201);
-    res.json({id: entityId});
-  });
+    // get request parameters
+    var productData = {
+        title: req.param('title'),
+        price: req.param('price'),
+        description: req.param('description'),
+    };
+    // process data
+    var redisClient = req.redisClient;
+    var product = new Product(productData, redisClient);
+    product.save(function (entityId) {
+        res.status(201);
+        res.json({id: entityId});
+    });
 });
 
 module.exports = router;
