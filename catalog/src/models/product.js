1b48aabd7118f863d35ff945978c5a4a821c16e3
Salvo Zappalà <salvo.a.zappala@gmail.com>
2016-03-09 18:27:12 +0100

    Fix indentation

--- catalog/src/models/product.js
+++ catalog/src/models/product.js
@@ -2,8 +2,8 @@ var Entity = require('./entity');
 var util = require('util');
 
 var Product = function (data, redisClient) {
-  Entity.apply(this, [data, redisClient]);
-  this.type = 'product';
+    Entity.apply(this, [data, redisClient]);
+    this.type = 'product';
 };
 
 util.inherits(Product, Entity);
