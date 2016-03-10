0281157882dd8a57db9f256a31ee6f8ad6a74278
Salvo Zappalà <salvo.a.zappala@gmail.com>
2016-03-10 20:03:40 +0100

    Fix indentation

--- catalog/src/routes/index.js
+++ catalog/src/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  redisClient = req.redisClient;
-  res.send('Hamaca catalog service\n');
+    redisClient = req.redisClient;
+    res.send('Hamaca catalog service\n');
 });
 
 module.exports = router;
