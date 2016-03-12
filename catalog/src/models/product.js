fe9978bf3e909af65fcbdf5c2192d27b1c870d71
Salvo Zappalà <salvo.a.zappala@gmail.com>
2016-03-12 15:42:29 +0100

    Move loadRange to product entity

--- catalog/src/models/product.js
+++ catalog/src/models/product.js
@@ -21,7 +21,29 @@ Product.prototype.validate = function () {
 };
 
 Product.loadRange = function (startRange, endRange, redisClient, done) {
-    Entity.loadRange('product', startRange, endRange, redisClient, done);
+    if (typeof startRange !== 'number' ||
+            typeof endRange !== 'number' ||
+            startRange < 1 ||
+            endRange < startRange) {
+        throw 'Invalid entity range';
+    }
+    var i = startRange;
+    var products = [];
+    // TODO don't try to load non existing products
+    do {
+        var key = 'product:' + i;
+        redisClient.hgetall(key, function (err, res) {
+            var productData = {};
+            for (var dataKey in res) {
+                productData[dataKey] = res[dataKey];
+            }
+            products.push(new Product(productData));
+            if (products.length === endRange) {
+                done(products);
+            }
+        });
+        i++;
+    } while (i <= endRange);
 };
 
 module.exports = Product;
