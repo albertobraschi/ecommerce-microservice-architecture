0bc6e85b73affc4cd39f04e78162dfa3636a3f72
Salvo Zappalà <salvo.a.zappala@gmail.com>
2016-03-21 21:13:31 +0100

    Return 400 Bad Request if the cart is missing

--- checkout/src/routes/checkout.js
+++ checkout/src/routes/checkout.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 var Checkout = require('../models/checkout');
 var DataStore = require('../models/dataStore');
+var CatalogService = require('../models/catalogService');
 
 /*
  * Start a new checkout process
  */
 router.post('/', function (req, res) {
-    res.sendStatus(404);
+    var cart = req.body.cart;
+    if (typeof cart !== 'object' || cart.length < 1) {
+        res.sendStatus(400);
+    }
 });
 
 module.exports = router;
