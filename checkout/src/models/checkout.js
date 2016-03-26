6b1a299706fd963b046ba2c7e40829a7843ca605
Salvo Zappalà <salvo.a.zappala@gmail.com>
2016-03-26 14:01:44 +0100

    Add shipping in Checkout model

--- checkout/src/models/checkout.js
+++ checkout/src/models/checkout.js
@@ -1,11 +1,21 @@
-var Checkout = function (cart) {
-    this.cart = cart;
+var Checkout = function (cart, shipping) {
+    if (typeof cart !== 'undefined') {
+        this.cart = cart;
+    }
+    if (typeof shipping !== 'undefined') {
+        this.shipping = shipping;
+    }
 };
 
 Checkout.prototype.cart = {};
+Checkout.prototype.shipping = {};
 
 Checkout.prototype.save = function (dataStore, done) {
     return dataStore.saveCheckout(this, done);
 };
+
+Checkout.prototype.setShipping = function (shipping) {
+    this.shipping = shipping;
+}
 
 module.exports = Checkout;
